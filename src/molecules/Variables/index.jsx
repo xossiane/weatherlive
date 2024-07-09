2ad8d499d045e7d3f7e00c946905c85711168d32
Josiane Lima <anejosi.lima@gmail.com>
2024-07-09 17:16:43 -0300

    Add: variables on component

--- src/molecules/Variables/index.jsx
+++ src/molecules/Variables/index.jsx
@@ -8,8 +8,11 @@ function Variables({data}) {
     <li>Temperatura atual: {data.main.temp}ºC </li>
     <li>Temperatura máxima: {data.main.temp_max}ºC </li>
     <li>Temperatura mínima: {data.main.temp_min}ºC </li>
+    <li>Descrição do tempo: {data.weather[0].description} </li>
+    <li>Temperatura mínima: {data.main.temp_min}ºC </li>
     <li>Pressão: {data.main.pressure} hpa </li>
     <li>Umidade: {data.main.humidity}% </li>
+    <li>Velocidade do Vento: {data.wind.speed}km/h </li>
   </ul>
   </>
   )
