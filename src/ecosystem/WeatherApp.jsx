f47134802bea508b7a0fba52b84a7b99ced26a46
Josiane Lima <anejosi.lima@gmail.com>
2024-07-14 14:26:45 -0300

    Fix: description

--- src/ecosystem/WeatherApp.jsx
+++ src/ecosystem/WeatherApp.jsx
@@ -77,6 +77,7 @@ export default function WeatherApp() {
     } else {
       setLoading(false);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [lat, long]);
 
   const handleIconClick = () => {
